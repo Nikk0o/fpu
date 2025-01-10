59c6aab3f81c5d9c7623d07e448716eb1473a368
Nikk0o <leroishy@gmail.com>
2025-01-10 01:31:31 -0300

    Finished (but not tested) float_to_double

--- float to double/float_to_double.v
+++ float to double/float_to_double.v
@@ -6,11 +6,11 @@ module float_to_double(
                        nan_exception);
 
     reg sign_32;
-    reg [7:0] exp_32;
+    reg [7:0] signed exp_32;
     reg [22:0] mantissa_32;
 
     reg sign_64;
-    reg [10:0] exp_64;
+    reg [10:0] signed exp_64;
     reg [51:0] mantissa_64;
 
     reg [1:0] state;
@@ -18,6 +18,11 @@ module float_to_double(
     always @(posedge clk or negedge reset)
     begin
         if (!reset) begin
+            mantissa_64 <= 52'b0;
+            exp_64 <= 11'b0;
+            sign_64 <= 0;
+            state <= 2'b0;
+            nan_exception <= 0;
         end
         else begin
             case (state)
@@ -35,6 +40,7 @@ module float_to_double(
                         mantissa_64[49:29] <= mantissa_32[20:0];
                         mantissa_64[28:0] <= 29'b0;
                         nan_exception <= 1;
+                        state <= 2'b11;
                     end
                     else if (exp_32 == 8'b11111111 && mantissa_32[22] == 1'b1) begin
                     // qNaN
@@ -42,10 +48,31 @@ module float_to_double(
                         exp_64 <= 11'b11111111111;
                         mantissa_64[51:29] <= mantissa_32;
                         mantissa_64[28:0] <= 29'b0;
+                        nan_exception <= 0;
+                        state <= 2'b11;
                     end
+                    else if (exp_32 == 8'b11111111 && mantissa_32 == 23'b0) begin
+                        sign_64 <= sign_32;
+                        exp_64 <= 11'b11111111111;
+                        mantissa_64 <= 52'b0;
+                        nan_exception <= 0;
+                        state <= 2'b11;
+                    end
+                    else
+                        state <= 2'b10;
+                end
+                2'b10: begin: conversion
+                    sign64 <= sign_32;
+                    exp_64 <= exp_32;
+                    mantissa_64[51:29] <= mantissa_32;
+                    mantissa_64[28:0] <= 29'b0;
                 end
             endcase
         end    
     end
+
+    assign double[63] <= sign_64;
+    assign double[62:52] <= exp_64;
+    assign double[51:0] mantissa_64;
 
 endmodule
